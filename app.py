6626a12a2d4cc71fc0bd9156ac10dcdd0bbb6447
yuya yano <yuya.yano.6260@gmail.com>
2013-12-26 16:34:48 +0900

    Added files.

---
+++ app.py
@@ -0,0 +1,28 @@
+# -*- encoding: utf-8 -*-
+
+from flask import Flask, render_template, redirect, url_for, request
+import pusher
+
+app = Flask(__name__)
+
+app.config.from_pyfile("app.cfg")
+
+p = pusher.Pusher(
+    app_id = app.config["ID"],
+    key = app.config["KEY"],
+    secret = app.config["SECRET"]
+)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/pusher', methods=["POST"])
+def pusher():
+    if request.method == 'POST':
+        p['channel1'].trigger('event1', {'message': "((((((((((っ･ωΣ[柱]ｶﾞｺｯ!"})
+    return 'ok'
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='192.168.5.7', port=8080)
